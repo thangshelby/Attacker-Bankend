73744257436e5d512c8605bbbda3903c6a0c10b6
thangshelby <n.nducthangg@gmail.com>
2025-07-17 22:18:08 +0700

    test python route

--- services/express-service/src/routes/index.js
+++ services/express-service/src/routes/index.js
@@ -1,5 +1,7 @@
 import userRouter from "../routes/userRoute.js";
+import pythonRouter from "./pythonRoute.js";
 
 export default function routes(server) {
   server.use("/api/v1/user", userRouter);
+  server.use("/api/v1/python", pythonRouter);
 }
